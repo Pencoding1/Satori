440285360d32f84fc5deff85b7de6d65886b1f50
Tedomotoji <68549456+tedomotojirikikudo@users.noreply.github.com>
2021-08-15 23:35:49 +0700

    Mobile version
    Redesign GUI, add some vertical wallpapers.

--- main.py
+++ main.py
@@ -21,34 +21,49 @@ class Pefect(Popup):
 
 class Gift(Popup):
     pass
+    
+class Menu(Screen):
 
-class Loading(Screen):
-
+    btn = ObjectProperty(None)
+    lbl = ObjectProperty(None)
     img = ObjectProperty(None)
+    speaker = ObjectProperty(None)
 
     def callback(self):
-        '''Thực ra thì không cần thiết phải tạo loading screen bởi vì chẳng có gì phức tạp ở đây cả. Nhưng vì nó ngầu nên tôi thêm vào.
-        Actually, there's no need to create loading screen because there's nothing complex here. But it's so cool so I added it.'''
-        Clock.schedule_once(self.callback0, 2)
-        
-    def callback1(self):
-        '''Lấy đường dẫn ảnh từ cache.
-        Fetch the image path from cache.'''
-        cache.ran_img(False)
+        '''Cập nhật dữ liệu cho các button và label trong trường hợp đổi ngôn ngữ hoặc đoán số.
+        Update data for the buttons and the labels in case changing the language or guessing the number.'''
+        self.btn.text = cache.content[0]
+        self.lbl.text = '\n'.join(cache.content[1:5])
+        cache.ran_img(True)
         self.img.source = cache.img_source
         
-    def callback0(self, *args):
-        '''Chuyển sang màn hình menu chính.
-        Switch to the main menu screen.'''
+    def callback0(self, temp):
+        '''Tôi phải tạo hàm vì có vẻ như root.manager.transition không hoạt động bên trong file kv.
+        I had to create this function because root.manager.transition seems didn't work inside kv file.'''
         app = App.get_running_app()
-        app.root.transition.direction = 'left'
-        app.root.current = 'menu'
-
+        if temp == True:
+            app.root.transition.direction = 'up'
+            app.root.current = 'ask0'
+        else:
+            app.root.transition.direction = 'down'
+            app.root.current = 'loading'
+    
+    def callback1(self, state):
+        '''Đổi hình ảnh theo trạng thái button.
+        Change the image by the button state.'''
+        app = App.get_running_app()
+        if state == 'normal':
+            self.speaker.source = 'resources/speaker.png'
+        else:
+            self.speaker.source = 'resources/speaker_mute.png'
+        app.temp = state
+            
 class Result(Screen):
     
     btn = ObjectProperty(None)
     lbl0 = ObjectProperty(None)
     lbl1 = ObjectProperty(None)
+    img = ObjectProperty(None)
     
     def callback(self):
         '''Cập nhật dữ liệu cho các button và label trong trường hợp đổi ngôn ngữ hoặc đoán số.
@@ -56,15 +71,17 @@ class Result(Screen):
         self.lbl0.text = cache.content[10]
         self.lbl1.text = str(int(cache.num))
         self.btn.text = cache.content[11]
+        cache.ran_img(True)
+        self.img.source = cache.img_source
         
     def callback0(self):
         '''Lại lần nữa nào.
         Ah shit, here we go again.'''
         cache.reload()
         app = App.get_running_app()
-        app.root.transition.direction = 'right'
+        app.root.transition.direction = 'down'
         app.root.current = 'loading'
-        
+    
     def callback1(self):
         '''Kích hoạt Easter Egg nếu đạt điều kiện.
         Trigger the Easter Egg if the coditon is met.'''
@@ -104,7 +121,7 @@ class Final(Screen):
         self.lbl1.text = cache.content[10] + str(int(cache.num)) + cache.content[7]
         self.btn0.text = cache.content[8]
         self.btn1.text = cache.content[9]
-        
+    
     def callback0(self, temp):
         '''Ở câu hỏi cuối cùng, tôi phải dùng phương pháp loại trừ.
         In the last question, I had to use the exclusion method.'''
@@ -115,7 +132,7 @@ class Final(Screen):
         cache.save_config()
         app.root.transition.direction = 'left'
         app.root.current = 'result'
-    
+        
     def callback2(self):
         '''Lại lần nữa nào.
         Ah shit, here we go again.'''
@@ -123,42 +140,7 @@ class Final(Screen):
         app = App.get_running_app()
         app.root.transition.direction = 'right'
         app.root.current = 'loading'
-
-class Menu(Screen):
-
-    btn = ObjectProperty(None)
-    lbl = ObjectProperty(None)
-    speaker = ObjectProperty(None)
-
-    def callback(self):
-        '''Cập nhật dữ liệu cho các button và label trong trường hợp đổi ngôn ngữ hoặc đoán số.
-        Update data for the buttons and the labels in case changing the language or guessing the number.'''
-        self.btn.text = cache.content[0]
-        self.lbl.text = '\n'.join(cache.content[1:5])
-        if cache.count == 10:
-                Gift().open()
-        
-    def callback0(self, temp):
-        '''Tôi phải tạo hàm vì có vẻ như root.manager.transition không hoạt động bên trong file kv.
-        I had to create this function because root.manager.transition seems didn't work inside kv file.'''
-        app = App.get_running_app()
-        if temp == True:
-            app.root.transition.direction = 'left'
-            app.root.current = 'ask0'
-        else:
-            app.root.transition.direction = 'right'
-            app.root.current = 'loading'
             
-    def callback1(self, state):
-        '''Đổi hình ảnh theo trạng thái button.
-        Change the image by the button state.'''
-        app = App.get_running_app()
-        if state == 'normal':
-            self.speaker.source = 'resources/speaker.png'
-        else:
-            self.speaker.source = 'resources/speaker_mute.png'
-        app.temp = state
-
 class Ask(Screen):
 
     img = ObjectProperty(None)
@@ -181,13 +163,13 @@ class Ask(Screen):
         '''Đổi sang câu hỏi tiếp theo.
         Switch to the next question.'''
         app = App.get_running_app()
-        app.root.transition.direction = 'up'
+        app.root.transition.direction = 'left'
         cache.read_mind(temp)
         if cache.count_q == 10:
             app.root.current = 'final'
         else:
             app.root.current = self.temp
-   
+    
     def callback2(self):
         '''Lại lần nữa nào.
         Ah shit, here we go again.'''
@@ -196,16 +178,36 @@ class Ask(Screen):
         app.root.transition.direction = 'right'
         app.root.current = 'loading'
 
-class SatoriApp(App):
+class Loading(Screen):
 
+    img = ObjectProperty(None)
+
+    def callback(self):
+        '''Thực ra thì không cần thiết phải tạo loading screen bởi vì chẳng có gì phức tạp ở đây cả. Nhưng vì nó ngầu nên tôi thêm vào.
+        Actually, there's no need to create loading screen because there's nothing complex here. But it's so cool so I added it.'''
+        Clock.schedule_once(self.callback0, 2)
+        
+    def callback1(self):
+        '''Lấy đường dẫn ảnh từ cache.
+        Fetch the image path from cache.'''
+        cache.ran_img(False)
+        self.img.source = cache.img_source
+        
+    def callback0(self, *args):
+        '''Chuyển sang màn hình menu chính.
+        Switch to the main menu screen.'''
+        app = App.get_running_app()
+        app.root.transition.direction = 'up'
+        app.root.current = 'menu'
+
+class SatoriApp(App):
+    
     sound = ObjectProperty(None)
     temp = StringProperty('normal')
-
-    def build(self):
-        self.icon = 'resources/icon.png'
-        self.title = 'Satori Komeiji'
-        Window.size = (1280,720)
     
+    def build(self):
+        self.title = 'Satori Komeiji'
+        
     def callback(self, *args):
         '''Tôi phải viết hàm này vì nếu loading_screen là screen 0 thì nó sẽ không kích hoạt event on_enter.
         I had to create this function because if loading screen is screen 0, the event on_enter wouldn't trigger.'''
@@ -232,6 +234,6 @@ class SatoriApp(App):
             self.sound.play()
         else:
             self.sound.stop()
-            
+ 
 if __name__ == '__main__':
     SatoriApp().run()
