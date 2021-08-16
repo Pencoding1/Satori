c2f2c715cc161591edbdc7719529d963318787d5
Tedomotoji <68549456+tedomotojirikikudo@users.noreply.github.com>
2021-08-16 13:08:31 +0700

    add icon for apk

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ Config.set('kivy', 'default_font', ['SVN-Have Heart 2', 'resources/SVN-Have Hear
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.clock import Clock
