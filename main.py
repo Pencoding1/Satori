cadf60155bb5ce65ff9bd94d36c4355af92b97b3
Tedomotoji <68549456+tedomotojirikikudo@users.noreply.github.com>
2021-08-17 15:57:48 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.config import Config
 Config.set('kivy', 'default_font', ['SVN-Have Heart 2', 'resources/SVN-Have Heart 2.ttf'])
 
+import os, sys
+from kivy.resources import resource_add_path, resource_find
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
@@ -155,9 +157,9 @@ class Menu(Screen):
         Change the image by the button state.'''
         app = App.get_running_app()
         if state == 'normal':
-            self.speaker.source = 'resources/speaker.png'
+            self.speaker.source = cache.path + 'resources/speaker.png'
         else:
-            self.speaker.source = 'resources/speaker_mute.png'
+            self.speaker.source = cache.path + 'resources/speaker_mute.png'
         app.temp = state
 
 class Ask(Screen):
@@ -206,7 +208,7 @@ class SatoriApp(App):
         self.icon = 'resources/icon.png'
         self.title = 'Satori Komeiji'
         Window.size = (1280,720)
-        return Builder.load_file('resources/GUI.kv')
+        return Builder.load_file(cache.path + 'resources/GUI.kv')
     
     def callback(self, *args):
         '''Tôi phải viết hàm này vì nếu loading_screen là screen 0 thì nó sẽ không kích hoạt event on_enter.
@@ -236,4 +238,8 @@ class SatoriApp(App):
             self.sound.stop()
             
 if __name__ == '__main__':
+    if hasattr(sys, '_MEIPASS'):
+        resource_add_path(os.path.join(sys._MEIPASS))
+        cache.path = sys._MEIPASS
+    cache.init()
     SatoriApp().run()
